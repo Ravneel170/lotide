571b1d96edc72fc169d73c1a733e95ec0149401a
Ravneel <ravneelsingh17@gmail.com>
2023-03-02 00:08:54 -0500

    without.js

---
+++ without.js
@@ -0,0 +1,9 @@
+function filterArray(arr, predicate) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (predicate(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
